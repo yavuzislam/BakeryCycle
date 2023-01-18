c13bd67e0bae8bbb5c4d57d4bbe86654d2aa8a24
yavuzislam <yavuzislam@users.noreply.github.com>
2023-01-18 23:59:41 +0300

    continue

--- BakeryCycle/Assets/Scripts/Bake.cs
+++ BakeryCycle/Assets/Scripts/Bake.cs
@@ -15,8 +15,8 @@ public class Bake : MonoBehaviour
     {
         if (other.tag=="Player")
         {
-            count = stackParent.transform.childCount;
-            if (count!=stacks.stackList.Count)
+            int count = takeParent.transform.childCount;
+            for (int i = 0; i < count; i++)
             {
                 takeParent.transform.GetChild(0).transform.parent = stackParent.transform;
                 stackParent.transform.GetChild(stackParent.transform.childCount - 1).DOLocalMove(pos, 0.5f);
@@ -25,34 +25,4 @@ public class Bake : MonoBehaviour
             
         }
     }
-    private void OnTriggerExit(Collider other)
-    {
-        count = stacks.stackList.Count;
-    }
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.tag == "Player")
-    //    {
-    //        InvokeRepeating("TakeLeaven", 1f, 1f);
-    //    }
-    //}
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.tag == "Player")
-    //    {
-    //        CancelInvoke("TakeLeaven");
-    //    }
-    //}
-    //public void TakeLeaven()
-    //{
-    //    GameObject takeobje = Instantiate(bake_leaven, playerControl.transform);
-    //    takeobje.transform.localPosition = new Vector3(0f, 1f, 1f) + new Vector3(0f, 0.1f * stacks.stackList.Count, 0f);
-    //    takeobje.transform.localRotation = Quaternion.Euler(takeobje.transform.localRotation.x, 6f, takeobje.transform.localRotation.z);
-    //    stacks.stackList.Add(takeobje);
-
-    //    if (stacks.stackList.Count == 5)
-    //    {
-    //        CancelInvoke("TakeLeaven");
-    //    }
-    //}
 }
---
+++ BakeryCycle/Assets/Scripts/Stand.cs
@@ -0,0 +1,126 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class Stand : MonoBehaviour
+{
+    public stack stack;
+    int index = 0, count = 1, customer = 2;
+    public bool set, active, newpos;
+    float duration;
+    public GameObject stackparent;
+    public Transform standPos;
+    public List<Vector3> targetpos;
+    public Vector3 pos, customerParent;
+    public List<GameObject> customers;
+    private void Update()
+    {
+        if (active)
+        {
+            if (index == 0)
+            {                        //3
+                for (int i = 0; i < customers.Count; i++)
+                {
+                    Debug.Log(i);
+                    targetpos.Add(customers[i].transform.localPosition);
+                    Debug.Log(targetpos[i]);
+                }
+                active = false;
+            }
+            else
+            {
+                for (int i = count; i < customers.Count; i++)
+                {
+                    targetpos.Add(customers[i].transform.localPosition);
+                }
+                active = false;
+            }
+        }
+        if (newpos)
+        {
+            Debug.Log("index" + index);
+            if (index == 0)
+            {
+                for (int i = 1; i < customers.Count; i++)
+                {Debug.Log("girdi");
+                    customers[i].transform.DOLocalMove(targetpos[i - 1], 0.5f);
+                }
+                index++;
+                newpos = false;
+            }
+            else
+            {
+                for (int i = count + 1; i < customers.Count; i++)
+                {
+                    customers[i].transform.DOLocalMove(targetpos[i - customer], 0.5f);
+                }
+                customer++;
+                index++;
+                count++;
+                newpos = false;
+            }
+
+        }
+
+        if (set)
+        {
+            duration += Time.deltaTime / 2;
+            if (duration > 1)
+            {
+                duration = 0;
+                if (index == 0)
+                {
+                    active = true;
+                    standPos.GetChild(0).parent = customers[index].transform.GetChild(2);
+                    customers[index].transform.GetChild(2).GetChild(0).DOLocalMove(customerParent, 1.5f);
+                    newpos = true;
+                    customers[0].transform.DOLocalMove(customerParent, 20f);                   
+                    targetpos.Clear();
+
+                }
+                else
+                {
+                    active = true;
+                    if (standPos.childCount != 0)
+                    {
+                        standPos.GetChild(0).parent = customers[index].transform.GetChild(2);
+                        customers[index].transform.GetChild(2).GetChild(0).DOLocalMove(customerParent, 1.5f);
+                        newpos = true;
+                        customers[index].transform.DOLocalMove(customerParent, 20f);
+                        
+                        targetpos.Clear();
+                    }
+                    else
+                    {
+                        set = false;
+                        stack.stackList.Clear();
+                        pos = Vector3.zero;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            int count = stackparent.transform.childCount;
+            for (int i = 0; i < count; i++)
+            {
+                stackparent.transform.GetChild(0).parent = standPos;
+                standPos.GetChild(standPos.childCount - 1).transform.DOLocalMove(pos, 0.5f);
+                pos += new Vector3(0f, 0f, 0.01f);
+            }
+            if (standPos.childCount != 0)
+            {
+                set = true;
+            }
+        }
+    }
+}
